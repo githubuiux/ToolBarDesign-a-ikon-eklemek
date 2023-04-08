1efd10f839cbf663b012fbf9d246532b7d6ecad4
githubuiux <uiuxista@gmail.com>
2023-04-08 20:02:24 +0300

    Initial Commit

---
+++ ToolBarDesign/ToolBarDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolBarDesignApp.swift
+//  ToolBarDesign
+//
+//  Created by Levent on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToolBarDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
